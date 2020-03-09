dc80ffeda6598d4444173c06fa0988d2b6bb9c8e
Lindsay MacVean <lindsaymacvean@gmail.com>
2020-03-09 23:47:55 +0000

    added update cookie function to functions.php

--- functions.php
+++ functions.php
@@ -449,4 +449,9 @@ function html5_shortcode_demo_2($atts, $content = null) // Demo Heading H2 short
     return '<h2>' . $content . '</h2>';
 }
 
+function my_update_cookie( $logged_in_cookie ){
+    $_COOKIE[LOGGED_IN_COOKIE] = $logged_in_cookie;
+    }
+    add_action( ‘set_logged_in_cookie’, ‘my_update_cookie’ );
+
 ?>
