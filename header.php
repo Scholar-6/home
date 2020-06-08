11167169194193ed44b0a6abae4e0e98aad361ca
Lindsay MacVean <lindsaymacvean@gmail.com>
2020-06-08 17:07:44 +0100

    added new post button to menu

--- header.php
+++ header.php
@@ -157,29 +157,10 @@
 
                             <!-- USER INFORMATIONS -->
                             <?php
-                            /*
-                            * FRONT END CREATION
-                            */ 
-                            // CHECK IF USER CAN CREATE BLOG POST
-                            if ($woffice_role_allowed): ?>
-                                <?php
-                                /**
-                                 * Text of the button "Publish a new article" in the posts loop page
-                                 *
-                                 * @param string
-                                 */
-                                $new_blog_button_text = apply_filters('woffice_new_blog_article_button_text', __("New Blog Article", "woffice")); ?>
-                                
-                                <?php echo'<a href="#" class="btn btn-default frontend-wrapper__toggle" data-action="display" id="show-blog-create"><i class="fa fa-plus-square"></i> '. $new_blog_button_text .'</a>'; ?>
-                                <?php Woffice_Frontend::frontend_render('post',$frontend_process); ?>
-                            <?php endif; ?>
-                            
-                            <?php
                             // CHECK FROM OPTIONS
                             $header_user = woffice_get_settings_option('header_user');
                             if (is_user_logged_in()) :
                                 if ($header_user == "yep") : ?>
-                                test1
                                     <div id="nav-user" class="clearfix <?php echo (function_exists('bp_is_active')) ? 'bp_is_active' : ''; ?>">
                                         <a href="javascript:void(0);" id="user-thumb">
                                             <?php
@@ -198,10 +179,26 @@
                                         <?php endif; ?>
                                     </div>
                                 <?php else: ?>
-                                test2
                                     <div id="nav-user" class="clearfix <?php echo (function_exists('bp_is_active')) ? 'bp_is_active' : ''; ?>">
                                         <a href="<?php echo wp_logout_url() ?>" id="user-login"><i class="fa fa-sign-out-alt"></i></a>
                                     </div>
+                                <?php endif; ?>
+                                <?php
+                                /*
+                                * FRONT END CREATION
+                                */ 
+                                // CHECK IF USER CAN CREATE BLOG POST
+                                if ($woffice_role_allowed): ?>
+                                    <?php
+                                    /**
+                                     * Text of the button "Publish a new article" in the posts loop page
+                                     *
+                                     * @param string
+                                     */
+                                    $new_blog_button_text = apply_filters('woffice_new_blog_article_button_text', __("New Blog Article", "woffice")); ?>
+                                    
+                                    <?php echo'<a href="#" class="btn btn-default frontend-wrapper__toggle" data-action="display" id="show-blog-create"><i class="fa fa-plus-square"></i> '. $new_blog_button_text .'</a>'; ?>
+                                    <?php Woffice_Frontend::frontend_render('post',$frontend_process); ?>
                                 <?php endif; ?>
                             <?php else : ?>
                                 <div id="nav-user" class="clearfix <?php echo (function_exists('bp_is_active')) ? 'bp_is_active' : ''; ?>">
